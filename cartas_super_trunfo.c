2291e8e4693b5e95d1a6bd627ba3af76074af186
juliamamoraes <jm8839694@gmail.com>
2025-03-17 13:43:57 -0300

    Add files via upload

---
+++ cartas_super_trunfo.c
@@ -0,0 +1,41 @@
+
+--- Carta 1 ---
+Estado: pernambuco
+C├│digo: 81
+Cidade: olinda
+Popula├º├úo: 53
+PIB: 0.35 milh├Áes
+├ürea: 565215428608.00 km┬▓
+Pontos tur├¡sticos: 10
+Densidade Populacional: 0.00 hab/km┬▓
+PIB per capita: 0.01
+
+--- Carta 2 ---
+Estado: distrito federal
+C├│digo: 35
+Cidade: brasilia
+Popula├º├úo: 35620
+PIB: 0.60 milh├Áes
+├ürea: 8545546240.00 km┬▓
+Pontos tur├¡sticos: 5
+Densidade Populacional: 0.00 hab/km┬▓
+PIB per capita: 0.00
+
+--- Carta 3 ---
+Estado: acre
+C├│digo: 45
+Cidade: acre
+Popula├º├úo: 364541
+PIB: 0.90 milh├Áes
+├ürea: 21547944.00 km┬▓
+Pontos tur├¡sticos: 3
+Densidade Populacional: 0.02 hab/km┬▓
+PIB per capita: 0.00
+
+=== Super Trunfo: Cadastro de Cartas ===
+1. Cadastrar nova carta
+2. Exibir todas as cartas
+0. Sair
+Escolha uma op├º├úo:
+
+
